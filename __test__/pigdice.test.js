ff43587f16f2b007602184bb6c4c45d5877a88d3
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-01-31 10:42:04 -0800

    pass the test for Player()

--- __test__/pigdice.test.js
+++ __test__/pigdice.test.js
@@ -1,4 +1,17 @@
+import { Player } from './../src/pigdice.js';
 import { getRandomInt } from './../src/pigdice.js';
+
+describe('Player', () => {
+
+  test('should check for a new Player being created from constructor', () => {
+    let name = "player1";
+    let newPlayer = new Player(name);
+    expect(newPlayer.playerName).toBe("player1");
+    expect(newPlayer.score).toBe(0);
+    expect(newPlayer.currentScore).toBe(0);
+  });
+});
+
 
 describe('getRandomInt', () => {
 
